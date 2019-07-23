8b1965e21ef9f5a4ec3ca97371abc90191d63d28
wjt20040622 <53118016+wjt20040622@users.noreply.github.com>
2019-07-23 15:09:55 +0800

    Add files via upload

---
+++ if.py
@@ -0,0 +1,16 @@
+number = float(input("type something:"))
+if number == 1:
+    print("hello")
+elif number < 1:
+    print("surprise mother fucker")
+    if number!=0:
+        print("666")
+    else:
+        if number>1:
+            print("are you ZZ")
+            if  number !=233:
+                print("51452")
+
+
+
+
