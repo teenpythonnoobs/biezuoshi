10b8aa36d083e370e51cb12b9d188c2b822aad8f
linull2004 <53117621+linull2004@users.noreply.github.com>
2019-07-23 19:00:15 +0800

    Add files via upload

---
+++ game.py
@@ -0,0 +1,50 @@
+#coding:utf-8
+import random
+number = random.randint(0, 10000)
+'''print(number)'''
+def IsRight(guessnumber,number,time):
+	if guessnumber>10000 :
+		print(' should smaller than 10000')
+		return 0
+	elif guessnumber<0 :
+		print('should bigger than  than 0')
+		return 0
+	else:
+		if guessnumber<number:
+			print('smaller')
+			return 0
+		elif 	guessnumber>number:
+			print('bigger')
+			return 0
+		else:
+			
+			print('you win!')
+			return 1
+			
+		
+ok = 0
+message= 'input a number between 0 and 100000\n'
+time=0
+while (ok==0) :
+	if time>14:
+		print('you lose')
+		ok=1
+	else:
+		inputs=input(message)
+		try:
+			guessnumber = int(inputs)
+			if guessnumber is not None :
+				if isinstance(guessnumber,int):
+			
+					ok=IsRight(guessnumber,number,time)
+					time = time +1
+				else :
+					print("please input a number")
+		except :
+			if inputs=="exit":
+				ok=1
+			else:
+				print("please input a number")
+print('gameover')
+
+
