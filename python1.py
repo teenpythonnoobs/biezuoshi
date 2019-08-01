3f6243c7b6d374a1af523cecea28d203dcdb60bb
wjt20040622 <53118016+wjt20040622@users.noreply.github.com>
2019-08-01 18:01:39 +0800

    王家桐

---
+++ python1.py
@@ -0,0 +1,56 @@
+import random
+def sb(bb):
+	if bb == 'scissor' :
+		return 3
+	if bb == 'paper' :
+		return 5
+	if bb == 'stone' :	
+		return 4
+scissor=3
+stone=4
+paper=5
+ok=0
+a=random.choice(['3','4','5'])
+while(ok==0):
+	
+	inputs=input('put scissor=3\paper=4\stone=5 \n')
+	
+	if inputs == "EXit":
+		ok==1
+	
+	else:
+		
+		if inputs==a:
+			print("try again")
+		elif a >inputs:
+			if inputs==4:
+			  print("you lose")
+			elif inputs==3 and a==5:	
+				print('you win')
+			elif inputs==3 and a==4:
+				print("you lose")
+		elif a <inputs:
+			if inputs==5 and a==4:
+				print("you win")
+			elif inputs==5 and a==3:
+				print ("you lose")
+			elif inputs==4:
+				print("you win")
+	if ok==1:
+		again=2
+		while again==2 :
+				yesorno=input("Do you want to play again? (y/n)")			
+				if yesorno=="y" :
+					ok=0
+							
+				elif yesorno=="n" :
+					again=1
+				else : 
+					again=2
+		ok=again  				  
+
+	 													  
+		  	
+				
+		  
+	
