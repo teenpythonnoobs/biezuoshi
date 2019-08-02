f68b2ee36beb32caf4fd621645d04a72ba97e0eb
wjt20040622 <53118016+wjt20040622@users.noreply.github.com>
2019-08-02 21:26:41 +0800

    66

--- python1.py
+++ python1.py
@@ -1,10 +1,60 @@
+
 import random
+def sb(bb):
+	if bb == 'scissor' :
+		return 3
+	if bb == 'paper' :
+		return 5
+	if bb == 'stone' :	
+		return 4
+ok=0
+a=random.choice(['3','4','5'])
+while(ok==0):
+	inputs=input('put scissor=3\paper=4\stone=5|')
+	if(inputs==a):
+			print("try	again")
+	
+	else:
+		
+		while(inputs==5):
+			if a==3:
+				print("you	lose")		
+			elif a==4:
+				print('you	win')
+		while(inputs==4):
+			if a==3:
+				print("you	lose")
+			elif a==5:
+				print("you	lose")
+		while (inputs==3):
+			if a==4:
+				print("you win")
+			elif(a==5):
+				print("you win")
+		
+	if ok==1:
+		again=2
+		while (again)==2 :
+				yesorno=input("Do you want to play again? (y/n)")			
+				if yesorno=="y" :
+					ok=0
+							
+				elif yesorno=="n" :
+					again=1
+				else : 
+					again=2
+
+import random
+def sb(bb):
+	if bb == 'scissor' :
+		return 3
+	if bb == 'paper' :
+		return 5
+	if bb == 'stone' :	
+		return 4
 
 
 
-scissor=3
-stone=4
-paper=5
 ok=0
 a=random.choice(['3','4','5'])
 while(ok==0):
@@ -13,7 +63,7 @@ while(ok==0):
 	
 		if inputs == "EXit":
 			ok==1
-	
+if sb( )is not none:
 		else:
 		
 			if a==inputs:
@@ -49,8 +99,7 @@ while(ok==0):
 while again==2:
 	print("gameover")  				  
 
-	 													  
-		  	
-				
-		  
-	
+
+
+
+       	
