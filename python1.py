ac085436e2db9966eda21d74f47e2e30e901ffa2
wjt20040622 <53118016+wjt20040622@users.noreply.github.com>
2019-08-02 21:20:04 +0800

    326

--- python1.py
+++ python1.py
@@ -1,11 +1,7 @@
 import random
-def sb(bb):
-	if bb == 'scissor' :
-		return 3
-	if bb == 'paper' :
-		return 5
-	if bb == 'stone' :	
-		return 4
+
+
+
 scissor=3
 stone=4
 paper=5
@@ -13,40 +9,48 @@ ok=0
 a=random.choice(['3','4','5'])
 while(ok==0):
 	
-	inputs=input('put scissor=3\paper=4\stone=5 \n')
+		inputs=input('put scissor=3\paper=4\stone=5 ')
 	
-	if inputs == "EXit":
-		ok==1
+		if inputs == "EXit":
+			ok==1
 	
-	else:
+		else:
 		
-		if inputs==a:
-			print("try again")
-		elif a >inputs:
-			if inputs==4:
-				print("you lose")
-			elif inputs==3 and a==5:	
-				print('you win')
-			elif inputs==3 and a==4:
-				print("you lose")
-		elif a <inputs:
-			if inputs==5 and a==4:
-				print("you win")
-			elif inputs==5 and a==3:
-				print ("you lose")
-			elif inputs==4:
-				print("you win")
-	if ok==1:
-		again=2
-		while again==2 :
-				yesorno=input("Do you want to play again? (y/n)")			
-				if yesorno=="y" :
-					again=0
-							
-				elif yesorno=="n" :
-					again=1
-				else : 
-					again=2
-		ok=again  				  
+			if a==inputs:
+				print("try again")
+				ok==1
+			elif a >inputs:
+				if inputs==4:
+			  		print("you lose")
+				elif inputs==3 and a==5:	
+					print('you win')
+				elif inputs==3 and a==4:
+					print("you lose")
+					ok==1
+			elif a <inputs:
+				if inputs==5 and a==4:
+					print("you win")
+				elif inputs==5 and a==3:
+					print ("you lose")
+				elif inputs==4:
+					print("you win")
+					ok==1
+		if ok==1:
+			again=2
+			while again==2 :
+					yesorno=input("Do you want to play again? (y/n)")			
+					if yesorno=="y" :
+						ok=0
+					elif yesorno=="n" :
+						again=1
+					else : 
+						again=2
+			ok=again
+while again==2:
+	print("gameover")  				  
 
-
+	 													  
+		  	
+				
+		  
+	
