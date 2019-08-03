85684c2c51c333de8a33500a06f4fd925683f082
wjt20040622 <53118016+wjt20040622@users.noreply.github.com>
2019-08-03 17:58:22 +0800

    ssp

--- python1.py
+++ python1.py
@@ -1,105 +1,64 @@
 
+import os
 import random
 def sb(bb):
 	if bb == 'scissor' :
 		return 3
-	if bb == 'paper' :
+	elif bb == 'paper' :
 		return 5
-	if bb == 'stone' :	
+	elif bb == 'stone' :	
 		return 4
-ok=0
-a=random.choice(['3','4','5'])
-while(ok==0):
-	inputs=input('put scissor=3\paper=4\stone=5|')
-	if(inputs==a):
-			print("try	again")
+	elif bb == 'exit' :
+		return 6
 	
+win = False
+ok=False
+a=random.choice(['3','4','5'])
+while not ok:
+	bp=sb(input('put scissor paper stone\n'))
+	
+	
+	if bp==6 :
+		break
+	if(bp==a):
+		print("try	again")
+		ok = True
 	else:
-		
-		while(inputs==5):
+		if (bp==5):
 			if a==3:
-				print("you	lose")		
+				print("you	lose0")
+				ok = True		
 			elif a==4:
 				print('you	win')
-		while(inputs==4):
+				win = True
+		if (bp==4):
 			if a==3:
-				print("you	lose")
+				print("you	win")
+				win = True
 			elif a==5:
 				print("you	lose")
-		while (inputs==3):
+				ok = True
+		if  (bp==3):
 			if a==4:
-				print("you win")
+				print("you lose")
+				ok = True
 			elif(a==5):
 				print("you win")
+				win = True
 		
-	if ok==1:
-		again=2
-		while (again)==2 :
-				yesorno=input("Do you want to play again? (y/n)")			
+	if win:
+		againinputok=False
+		while  not againinputok :
+				yesorno=input("Do you want to play again? (y/n) \n")			
 				if yesorno=="y" :
-					ok=0
-							
+					ok=False
+					againinputok = True		
 				elif yesorno=="n" :
-					again=1
+					ok=True
+					againinputok = True
 				else : 
-					again=2
-
-import random
-def sb(bb):
-	if bb == 'scissor' :
-		return 3
-	if bb == 'paper' :
-		return 5
-	if bb == 'stone' :	
-		return 4
-
-
-
-ok=0
-a=random.choice(['3','4','5'])
-while(ok==0):
-	
-		inputs=input('put scissor=3\paper=4\stone=5 ')
-	
-		if inputs == "EXit":
-			ok==1
-if sb( )is not none:
-		else:
-		
-			if a==inputs:
-				print("try again")
-				ok==1
-			elif a >inputs:
-				if inputs==4:
-			  		print("you lose")
-				elif inputs==3 and a==5:	
-					print('you win')
-				elif inputs==3 and a==4:
-					print("you lose")
-					ok==1
-			elif a <inputs:
-				if inputs==5 and a==4:
-					print("you win")
-				elif inputs==5 and a==3:
-					print ("you lose")
-				elif inputs==4:
-					print("you win")
-					ok==1
-		if ok==1:
-			again=2
-			while again==2 :
-					yesorno=input("Do you want to play again? (y/n)")			
-					if yesorno=="y" :
-						ok=0
-					elif yesorno=="n" :
-						again=1
-					else : 
-						again=2
-			ok=again
-while again==2:
-	print("gameover")  				  
+					againinputok= False
 
 
 
 
-       	
